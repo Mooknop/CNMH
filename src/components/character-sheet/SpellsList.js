578255bad0d7a567040a2e8fde758560ca17e5c7
Stanislav <mooknop@gmail.com>
2025-04-26 18:11:02 -0700

    Wand!

--- src/components/character-sheet/SpellsList.js
+++ src/components/character-sheet/SpellsList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './SpellsList.css';
 import CollapsibleCard from '../shared/CollapsibleCard';
 import { 
